9106a8e532e576da5145f248d87563e4d7f3bebe
syoch <syoch64@gmail.com>
2023-03-19 11:55:04 +0900

    Update: move some definetion

--- CTF/2022/2nd/3.c
+++ CTF/2022/2nd/3.c
@@ -1,10 +1,12 @@
+#include <socket_server.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <kbhit.h>
 #include <inttypes.h>
+#include <stdbool.h>
 
-int f(int sock) {
+void f() {
   char buf[8] = {};
 
   int l = 0;
@@ -14,16 +16,15 @@ int f(int sock) {
   fflush(stdin);
 
   bool dbg = getenv("DEBUG");
-
   if (dbg) printf("Debugger is enabled.\n");
 
   printf("Reading the message...\n");
 
   char c = '\0';
   for (int i = 0; i < l; i++) {
-    read(sock, &c, 1);
+    read(0, &c, 1);
     if (c == '\n') {
-      printf("Warning: you inputed %d characters, %d characters remained\n", i,
+      printf("Warning: you inputted %d characters, %d characters remained\n", i,
              l - i);
       break;
     }
@@ -31,67 +32,6 @@ int f(int sock) {
   }
 
   printf("Welcome %s!\n", buf);
-  return 0;
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/un.h>
-
-void handle_errno(const char *name, int ret) {
-  printf("[Utilities] The syscall(%s) has successed.\n", name);
-  if (ret != 0) {
-    printf("[Utilities] The syscall(%s) has failed.\n", name);
-    printf("[Utilities]   errno = %d (%s)\n", errno, strerror(errno));
-    exit(1);
-  }
-}
-int main() {
-  int sock0 = socket(AF_LOCAL, SOCK_STREAM, 0);
-
-  struct sockaddr_un addr;
-  addr.sun_family = AF_LOCAL;
-  strcpy(addr.sun_path, "./sockets/2022_2nd_3");
-
-  remove("./sockets/2022_2nd_3");
-  handle_errno("bind", bind(sock0, (struct sockaddr *)&addr, sizeof(addr)));
-
-  printf("[CTF Problem Server] Canary: Waiting for connection...\n");
-
-  handle_errno("listen", listen(sock0, 30));
-
-  for (;;) {
-    struct sockaddr_in client;
-    socklen_t len = sizeof(client);
-    int sock = accept(sock0, (struct sockaddr *)&client, &len);
-
-    printf("[CTF Problem Server] Canary: Got new client %d\n", sock);
-    if (fork() == 0) {
-      close(0);
-      dup(sock);
-      close(1);
-      dup(sock);
-      close(2);
-      dup(sock);
-
-      f(sock);
-      printf("Ended your session.\n");
-
-      shutdown(sock, SHUT_RDWR);
-      close(sock);
-      exit(0);
-      return 0;
-    }
-  }
-
-  close(sock0);
-
-  return 0;
-}
+SOCKET_MAIN("Canary", "./sockets/2022_2nd_3", f)
