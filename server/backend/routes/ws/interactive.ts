712303a6298c5ec62ff651ab527b0c435c6b1c96
syoch <syoch64@gmail.com>
2023-03-27 13:47:28 +0900

    update: daitai impl

--- server/backend/routes/ws/interactive.ts
+++ server/backend/routes/ws/interactive.ts
@@ -1,15 +1,56 @@
 import { Context } from "@kapsonfire/bun-bakery";
-import { ServerWebSocket } from "bun";
+import { ServerWebSocket, WebSocketHandler } from "bun";
+import { Task } from "../../../datas/problem/metadata/tasks/task/task";
+import problems from "../../../datas/problem_manager";
+
 
 export const WEBSOCKET = {
   open: (ws: ServerWebSocket) => {
-    console.log('OPEN');
+    //@ts-ignore
+    const task: Task = ws.data.task;
+
+    task.onMessage((data) => {
+      ws.send(data);
+    });
+
+    task.onDisconnect(() => {
+      ws.send("[Wrapper] The app has exited\n");
+
+      ws.close();
+    });
   },
-  message: (ws: ServerWebSocket, message: string) => {
-    console.log('RCV:', message);
-    ws.send(`ECHO: ${message}`);
+  message(ws: ServerWebSocket, message: string | Uint8Array) {
+    //@ts-ignore
+    const task: Task = ws.data.task;
+
+    const str = [...message].map(x => typeof x === 'number' ? String.fromCharCode(x) : x).join('');
+    task.writeStdin(str);
+  },
+  close(ws: ServerWebSocket) {
+    //@ts-ignore
+    const task: Task = ws.data.task;
+    task.kill();
   },
   upgrade: (ctx: Context) => {
-    ctx.acceptWebsocketUpgrade();
+    const hash = ctx.url.searchParams.get('hash');
+    if (!hash) {
+      ctx.sendResponse(new Response('hash is not specified', { status: 400 }));
+      return;
+    }
+
+    const problem = problems.getProblemWithHash(hash);
+    if (!problem) {
+      ctx.sendResponse(new Response('problem not found', { status: 404 }));
+      return;
+    }
+
+    const task = problem.runtime();
+
+    //@ts-ignore
+    ctx.acceptWebsocketUpgrade({
+      data: {
+        task
+      }
+    });
   }
 }
