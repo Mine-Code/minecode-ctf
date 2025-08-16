d765c7e7975e974ae5c60b29042d0265c70609b2
syoch <syoch64@gmail.com>
2025-08-17 03:47:10 +0900

    Chore: Removed script-land problem initialization

--- run_server.sh
+++ run_server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 set -e
-
-PROBLEM_CONTAINERS=""
 
 untar_problems() {
   printf "\e[1;32mUntaring problems...\e[0m\n"
@@ -26,62 +23,12 @@ untar_problems() {
   done
 }
 
-init_problems() {
-  printf "\e[1;32mInitializing all problems\e[0m\n"
-  for problem in $(echo $PROBLEMS | tr ',' '\n'); do
-    p=$PWD
-    printf "\e[32m  Initializing at $problem\e[0m\n"
-
-    cd $problem
-
-    CID=$(docker run -div $PWD:/mnt minecode-ctf-runner .mc_ctf/init.sh &)
-    printf "\e[2;32m    => $CID\e[0m\n"
-
-    cd $p
-  done
-  printf "\e[1;33mWaiting initializing process\e[0m\n"
-  wait
-}
-
-start_problem_daemons() {
-  printf "\e[1;32mStarting daemon processes\e[0m\n"
-  for problem in $(echo $PROBLEMS | tr ',' '\n'); do
-    p=$PWD
-    printf "\e[32m  Starting at $problem\e[0m\n"
-
-    cd $problem
-
-    CID=$(docker run -ditv $PWD:/mnt minecode-ctf-runner .mc_ctf/daemon.sh)
-    PROBLEM_CONTAINERS="$PROBLEM_CONTAINERS $CID"
-    printf "\e[2;32m    => $CID\e[0m\n"
-
-    cd $p
-  done
-}
-cleanup () {
-  printf "\e[1;32mRestore metadata.json owner\e[0m\n"
-  find . -name "metadata.json" | xargs sudo chown $USER
-
-  printf "\e[1;32mStopping Daemon tasks\e[0m\n"
-  for cid in $PROBLEM_CONTAINERS; do
-    printf "\e[32m  Stopping $cid\e[0m\n"
-    docker stop $cid &
-  done
-
-  wait
-}
-trap cleanup EXIT
-
 untar_problems
 
-export PROBLEMS=$(find problems -name "metadata.json" | xargs dirname | tr '\n' ',' | sed 's/,$//')
-
-init_problems
-
-start_problem_daemons
 
 printf "\e[1;32mSetup metadata.json owner\e[0m\n"
 find . -name "metadata.json" | xargs sudo chown $USER
 
 printf "\e[1;32mStarting main program\e[0m\n"
-set +e; bun server/main.ts; set -e
+export PROBLEMS=$(find problems -name "metadata.json" | xargs dirname | tr '\n' ',' | sed 's/,$//')
+bun server/main.ts
