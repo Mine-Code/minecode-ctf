d4f39fb60a6fd6c5a3ef148a57ba8c35de9d0def
syoch <syoch64@gmail.com>
2025-08-17 18:05:02 +0900

    Update: Refactor problem provider to enhance path validation and improve readability

--- server/ctf/problem_provider.ts
+++ server/ctf/problem_provider.ts
@@ -1,16 +1,30 @@
+import { normalize } from "path";
 import { IProblem, ProblemV1 } from "./problem";
 import { DockerWorker } from "./worker";
+import { existsSync } from "fs";
 export type ProblemProvider = () => Promise<IProblem[]>;
 
+// MC-CTF の問題形式に則っているかを検査する
+function isMineCodeCTFProblem(basepath: string): boolean {
+  return (
+    existsSync(basepath) &&
+    existsSync(`${basepath}/.mc_ctf/daemon.sh`) &&
+    existsSync(`${basepath}/.mc_ctf/init.sh`) &&
+    existsSync(`${basepath}/.mc_ctf/runtime.sh`)
+  );
+}
+
 export const problemFromEnv = async () => {
-  const env_problems = process.env.PROBLEMS ?? "";
-  const problem_basepaths = env_problems
+  const envProblems = process.env.PROBLEMS ?? "";
+  const problemBasepaths = envProblems
     .split(",")
-    .filter((x) => x.trim() !== "");
+    .filter((x) => !x.includes(".."))
+    .map((x) => normalize(x.trim()))
+    .filter((x) => x !== "" && isMineCodeCTFProblem(x));
 
   let problems: IProblem[] = [];
 
-  for (const basepath of problem_basepaths) {
+  for (const basepath of problemBasepaths) {
     console.group(`Loading problem from ${basepath}`);
 
     const worker = new DockerWorker("minecode-ctf-runner", basepath);
