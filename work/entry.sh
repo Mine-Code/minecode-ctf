e83661c61a4fcdb0c9cc86d50cadb639bdbf44a2
syoch <syoch64@gmail.com>
2025-08-18 18:20:31 +0900

    Remove: Removed work service

--- work/entry.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/tmp/setup_authorized_keys.sh
-
-/usr/sbin/sshd -D
--- work/setup_authorized_keys.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for github_user in yukikamome316 syoch; do
-    curl -L https://github.com/$github_user.keys >> ~/.ssh/authorized_keys
-done
-
-chmod 600 ~/.ssh/authorized_keys
